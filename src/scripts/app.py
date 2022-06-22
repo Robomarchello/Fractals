4d17531d1b81e8e2aa1cda4ddcca868759ad58c8
Marko Lialiuk <49954445+Robomarchello@users.noreply.github.com>
2022-06-22 13:43:34 +0300

    Sierpinski carpet (needs to be cleared)

--- src/scripts/app.py
+++ src/scripts/app.py
@@ -12,12 +12,16 @@ class App():
         self.fps = fps
         self.clock = pygame.time.Clock()
 
+        self.sCarpet = sCarpet(ScreenSize, 4)
 
     def loop(self):
         screen = self.screen
         while True:
             self.clock.tick(self.fps)
+
+            
             screen.fill((255, 255, 255))
+            self.sCarpet.draw(screen)  
 
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -25,3 +29,5 @@ class App():
                     raise SystemExit
 
             pygame.display.update()
+
+#[<rect(64, 64, 64, 64)>, <rect(192, 64, 64, 64)>, <rect(320, 64, 64, 64)>, <rect(64, 192, 64, 64)>, <rect(320, 192, 64, 64)>, <rect(64, 320, 64, 64)>, <rect(192, 320, 64, 64)>, <rect(320, 320, 64, 64)>]
---
+++ src/scripts/fractals/sCarpet.py
@@ -0,0 +1,49 @@
+from numpy import square
+import pygame
+
+class sCarpet:
+    def __init__(self, ScreenSize, steps):
+        self.ScreenSize = ScreenSize
+        self.color = pygame.Color('black')
+        self.steps = steps
+
+    def draw(self, screen):
+        squares = [
+            pygame.Rect(0, 0, self.ScreenSize[0] // 3, self.ScreenSize[1] // 3)
+            ]
+        squares[0].center = (self.ScreenSize[0] // 2, self.ScreenSize[1] // 2)
+
+        pygame.draw.rect(screen, self.color, squares[0])
+
+        for step in range(self.steps):
+            new_squares = []
+            for square in squares:
+                new_squares += self.slice_square(square)
+                
+            squares = new_squares.copy()
+
+            for square in new_squares:
+                pygame.draw.rect(screen, self.color, square)
+                
+    def slice_square(self, rect):
+        '''Splits a square into 8 3 times smaller squares😅'''
+
+        squares = []
+        cutSize = (rect.width // 3, rect.height // 3)
+        poses = [
+            pygame.Vector2(-rect.width, -rect.height),
+            pygame.Vector2(0, -rect.height),
+            pygame.Vector2(rect.width, -rect.height),
+            pygame.Vector2(-rect.width, 0),
+            pygame.Vector2(rect.width, 0),
+            pygame.Vector2(-rect.width, rect.height),
+            pygame.Vector2(0, rect.height),
+            pygame.Vector2(rect.width, rect.height),
+        ]
+
+        for position in poses:
+            new_rect = pygame.Rect((0, 0), cutSize)
+            new_rect.center = rect.center + position
+            squares.append(new_rect.copy())
+
+        return squares
